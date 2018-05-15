d3e125448d1af2a52bb70c168aefcd507493359b
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-05-15 14:45:21 +0100

    Wait for hosts to start before running Ansible.

---
+++ tests/docker/run-runner.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+cd tests
+
+hosts="$(ansible targets -i inventory.ini --list-hosts | tail --lines=+2 | sed -e 's/ //g')"
+
+for host in ${hosts}; do
+    >&2 echo "Waiting for host: ${host}"
+    /opt/wait-for-it.sh "${host}:22"
+    /opt/wait-for-it.sh "${host}:5432"
+done
+
+ansible-playbook -i inventory.ini test.yml
